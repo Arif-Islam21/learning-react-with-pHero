87bc5cd005f6f967199d0937c44ec3fc44318fa5
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-07 22:01:16 +0600

    declared my component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,22 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      {/* <p>learned about basic component </p>
+      <p>there are 4 type to declare component</p> */}
     </>
+  );
+}
+
+function Person() {
+  const age = 18;
+  const money = 5;
+  return (
+    <h3>
+      I am a Person with age {age} have {money} tk
+    </h3>
   );
 }
 
