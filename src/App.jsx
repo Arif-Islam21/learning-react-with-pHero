c14783cc58ea88bbc11bd7d1366e5aa9dc58906c
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-08 21:26:39 +0600

    working with click handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,15 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("Button Clicked");
+  }
+
   return (
     <>
-      <h1>Vite + React</h1>
-      <Device name="Laptop" price="55"></Device>
-      <Device name="Mobile" price="15"></Device>
-      <Student></Student>
-      <Developer></Developer>
-      {/* <p>learned about basic component </p>
-      <p>there are 4 type to declare component</p> */}
+      <h3>React Core Concepts 2</h3>
+      <button onClick={handleClick}>Click Me</button>
     </>
-  );
-}
-
-function Student() {
-  return (
-    <div className="student">
-      <h3>this is a student</h3>
-    </div>
-  );
-}
-
-function Device(props) {
-  const { name, price } = props;
-  return (
-    <h2>
-      This Device: {name} Price : {price}
-    </h2>
-  );
-}
-
-function Developer() {
-  const developerStyle = {
-    margin: "10px",
-    padding: "10px",
-    border: "2px solid tomato",
-    borderRadius: "10px",
-  };
-  return (
-    <div style={developerStyle}>
-      <h5>devo devo</h5>
-      <p>Coding</p>
-    </div>
   );
 }
 
