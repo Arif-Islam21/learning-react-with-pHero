42326ebf2cfa3696fad55210f0be03e18d5a5f69
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-08 21:35:49 +0600

    learned about some event handler

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,26 @@ function App() {
   function handleClick() {
     alert("Button Clicked");
   }
+  const handleClick2 = () => {
+    alert("Button 2 clicked");
+  };
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
 
   return (
     <>
       <h3>React Core Concepts 2</h3>
       <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>CLick 2</button>
+      <button onClick={() => addToFive(9)}>Add To Five</button>
+      <button
+        onClick={() => {
+          alert("thired clicked");
+        }}
+      >
+        Clikc 3
+      </button>
     </>
   );
 }
