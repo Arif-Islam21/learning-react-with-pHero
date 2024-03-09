ab142fa8845598c0e7bb0a64e831751599862ceb
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-09 23:20:06 +0600

    learned some advanced thing

--- src/Check.jsx
+++ src/Check.jsx
@@ -10,11 +10,16 @@ export default function Check() {
     const newReomve = count - 1;
     setCount(newReomve);
   };
+  const addFive = (num) => {
+    const newNumber = num + 5;
+    setCount(newNumber);
+  };
   return (
     <div>
       <h3>Hello From React World {count}</h3>
       <button onClick={handleAdd}>Add</button>
       <button onClick={handleRemove}>Remove</button>
+      <button onClick={() => addFive(2)}>Add Five</button>
     </div>
   );
 }
